580a89247790b5391709303a3fed5ea9af4fd103
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-04-12 15:11:54 -0700

    ShowroomCamera source.

---
+++ test_package/src/index.js
@@ -0,0 +1,27 @@
+import { ShowroomCameraDemo } from "app_package";
+
+document.body.style.width = "100%";
+document.body.style.height = "100%";
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+
+const title = document.createElement("p");
+title.innerText = "Showroom Camera Demo";
+title.style.fontSize = "32pt";
+title.style.textAlign = "center";
+document.body.appendChild(title);
+
+const div = document.createElement("div");
+div.style.width = "60%";
+div.style.margin = "0 auto";
+div.style.aspectRatio = "16 / 9";
+document.body.appendChild(div);
+
+const canvas = document.createElement("canvas");
+canvas.id = "renderCanvas";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+canvas.style.display = "block";
+div.appendChild(canvas);
+
+ShowroomCameraDemo.Run(canvas);
